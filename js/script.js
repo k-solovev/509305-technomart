377cb3702e6432eec8a7f52c1b88ed6c5b52c95d
k-solovev <k.solovev.work@gmail.com>
2018-04-06 18:30:52 +0300

    js2

---
+++ js/script.js
@@ -0,0 +1,26 @@
+    var link = document.querySelector(".contact-button");
+    var link_map = document.querySelector(".open-map");
+    var write_us = document.querySelector(".write-us-popup");
+    var map = document.querySelector(".map-popup");
+    var close = write_us.querySelector(".close-modal");
+    var close_map = document.querySelector(".map-popup .close-modal");
+
+    link.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      write_us.classList.add("modal-show");
+    });
+
+    close.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      write_us.classList.remove("modal-show");
+    });
+
+    link_map.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      map.classList.add("modal-show");
+    });
+
+    close_map.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      map.classList.remove("modal-show");
+    });
